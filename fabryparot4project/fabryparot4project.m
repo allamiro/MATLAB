39ed943bc30beebf77cfde0da2c105b0f8bee82d
Tamir Suliman <allamiro@gmail.com>
2025-01-06 02:34:34 +0100

    Update fabryparot4project.m

--- fabryparot4project/fabryparot4project.m
+++ fabryparot4project/fabryparot4project.m
@@ -1,43 +1,58 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Fabry-Perot: Transmitted Intensity vs. Phase K
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear; close all; clc;
+
+% Define phase range
 K = -10:0.01:10;
-r1 = 0.75;
-F = 4 * r1^2 / (1 - r1^2)^2;
-I1 = 1 ./ (1 + F * sin(K /2).^2);
-figure(1)
 
-plot(K , I1 , 'linewidth' , 2 , 'color' , [0.8 , 0.1 ,0.8]);
-grid on
-hold on
-r2 = 0.85;
-F = 4 * r2^2 / (1 - r2^2)^2;
-I2 = 1 ./ (1 + F * sin(K /2).^2);
+% Helper function for transmitted intensity
+fp_intensity = @(r, K) 1 ./ (1 + (4*r.^2 ./ (1-r.^2).^2) .* sin(K/2).^2);
 
-plot(K , I2 , 'linewidth' , 2 , 'color' , [0.1 , 0.7 ,0.7]);
-hold on
-r3 = 0.99;
-F = 4 * r3^2 / (1 - r3^2)^2;
-I3 = 1 ./ (1 + F * sin(K /2).^2);
+% Mirror reflectivities to compare
+reflectivities = [0.75, 0.85, 0.99];
+colors = [0.8 0.1 0.8; 0.1 0.7 0.7; 0.0 0.0 0.0]; % For plotting
 
-plot(K , I3 );
-axis([-10,10, 0 , 1.1]);
-xlabel('K = fxtaw=2 k d \mu cos \theta');
+figure('Name','Fabry-Perot Transmitted Intensity');
+hold on; grid on;
+
+for i = 1:length(reflectivities)
+    r = reflectivities(i);
+    I = fp_intensity(r, K);
+    plot(K, I, 'LineWidth', 2, 'Color', colors(i,:), ...
+        'DisplayName',['r = ', num2str(r)]);
+end
+
+axis([-10, 10, 0, 1.1]);
+xlabel('K = 2 k d \mu cos(\theta)');
 ylabel('I(K)');
-title('The Transmitted Intensity of a Fabry-Perot Interferometer');
-figure(2)
-r11=.5:.005:.99;
-FF = 3.14 * sqrt(r11) ./ (1 - r11);
-plot(r11,FF)
-xlabel('Mirror Reflectivity R');
-ylabel('Finesse FR');
-title('Reflectivity Finesse versus Mirorr Reflectivity R');
-grid on
-figure(3)
-FFC = 4 * r11.^2 ./ (1 - r11.^2).^2;
-plot(r11,FFC)
-ylim([-20 800]) 
-xlabel('Mirror Reflectivity R');
-ylabel('Contrast Factor FFC');
-title('Contast Factor versus Mirorr Reflectivity R');
-grid on
+title('Transmitted Intensity of a Fabry-Perot Interferometer');
+legend('show');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Finesse vs. Mirror Reflectivity
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure('Name','Finesse vs. Reflectivity'); 
+hold on; grid on;
 
+rVals = 0.5:0.005:0.99;
+F = pi * sqrt(rVals) ./ (1 - rVals); % Standard approximate finesse
+
+plot(rVals, F, 'LineWidth', 2, 'Color', [0.2 0.5 0.9]);
+xlabel('Mirror Reflectivity, R');
+ylabel('Finesse, F');
+title('Finesse vs. Mirror Reflectivity');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Contrast Factor vs. Mirror Reflectivity
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure('Name','Contrast Factor vs. Reflectivity');
+hold on; grid on;
+
+FFC = 4 * rVals.^2 ./ (1 - rVals.^2).^2; % Contrast factor
+plot(rVals, FFC, 'LineWidth', 2, 'Color', [0.9 0.3 0.3]);
+ylim([-20, 800]);
+xlabel('Mirror Reflectivity, R');
+ylabel('Contrast Factor, FFC');
+title('Contrast Factor vs. Mirror Reflectivity');
